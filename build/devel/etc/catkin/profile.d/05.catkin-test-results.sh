2e94292b25a41574a43416d1a6ef27824a0af30d
Guillaume de Chambrier <guillaume.dechambrier@epfl.ch>
2015-10-30 18:12:42 +0100

    added script file

--- build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/guillaume/roscode/catkin_ws/src/netft_rdt_driver/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
